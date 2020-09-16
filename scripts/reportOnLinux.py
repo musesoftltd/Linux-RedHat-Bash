f402dae7b5861a89a12acd527f771a4b424ce6f9
x <x@jenkins.musesoft>
2020-09-16 12:38:53 +0100

    add base report on linux

---
+++ scripts/reportOnLinux.py
@@ -0,0 +1,33 @@
+'''
+Created on 11 Oct 2017
+
+@author: ...
+'''
+import datetime
+
+from library.auditing import auditingLibrary
+from library.auditing.auditServerBase import auditServersBaseAudit
+from library.auditing.auditingLibrary import auditInitAudit, auditReport
+from linuxProperties import linuxPropertiesDict
+from auditServers import auditServers
+
+def completeChecks(env, serverList, propertiesDict, bApplyRequiredChanges):
+    # merge global propertiesDict into dict - deliberately overwriting local with global dict all values
+    runtimeProperties = dict()
+    #runtimeProperties.update(globalDictionary)
+    runtimeProperties.update(propertiesDict)
+
+    for servername in serverList :
+        auditServersBaseAudit(env, servername, runtimeProperties["usernamePega"], propertiesDict, bApplyRequiredChanges)
+
+        auditingLibrary.auditObjectAtoms.append(auditingLibrary.auditObjectAtomCompleteAnAction(servername, runtimeProperties["usernamePega"], runtimeProperties["identityFileFullPath"], runtimeProperties["identityFilePassword"], 'JVM Mem Opts', 'source ~/.bash_profile; grep -ia \'Xmx\' \'/opt/jboss/EAP-6.4.0/bin/standalone.conf\''))
+    
+        auditReport(env, servername)
+
+bApplyRequiredChanges = False
+
+auditInitAudit("PreProd Servers", "Base Linux")
+
+completeChecks('preProdServers', ['localhost', '127.0.0.1'], linuxPropertiesDict, bApplyRequiredChanges)
+
+exit()
